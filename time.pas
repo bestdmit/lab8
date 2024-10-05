fad8add8b469fa947a8b9d4ebb180b24029c1acb
bestdmit <bestdmit@gmail.com>
2024-10-05 06:27:23 +0300

    zadanie2

--- time.pas
+++ time.pas
@@ -3,9 +3,9 @@ var a := readinteger('Введите год');
 if (a mod 4 = 0) then
 begin
   if (a mod 100 = 0) and (a mod 400 <> 0) then
-     println('не високосный')
+     println('не високосный', 365)
   else
-     println('високосный')
+     println('високосный', 366)
 end
 else
-  println('не високосный')
+  println('не високосный', 366)
